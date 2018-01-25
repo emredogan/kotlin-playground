ca1d034eae778882773e8da1b50d600ca62720c7
Emre <emredogan.boun@gmail.com>
2018-01-26 00:20:15 +0100

    first commit

---
+++ app/src/main/java/com/fun/kotlin/kotlinbasic/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.`fun`.kotlin.kotlinbasic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var count = 0
+
+    fun reset(view: View) {
+
+        count = 0
+        textView.text = count.toString()
+
+    }
+
+    fun plusOne(view: View) {
+
+        count++
+        textView.text = count.toString()
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var textView = findViewById<TextView>(R.id.textView)
+
+        textView.text = "0"
+
+
+
+
+    }
+}
